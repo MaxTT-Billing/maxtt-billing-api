4692f3bed864bef9abee5a1b62b49a875ff32049
MaxTT-Billing <treadstonesolutions2023@gmail.com>
2025-09-04 17:26:22 +0530

    Delete migration directory

--- migration/2025-09-04_add_invoice_columns.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE invoices
-  ADD COLUMN IF NOT EXISTS invoice_seq integer,
-  ADD COLUMN IF NOT EXISTS franchisee_code text,
-  ADD COLUMN IF NOT EXISTS invoice_number_norm text,
-  ADD COLUMN IF NOT EXISTS customer_code text,
-  ADD COLUMN IF NOT EXISTS hsn_code varchar(8);
