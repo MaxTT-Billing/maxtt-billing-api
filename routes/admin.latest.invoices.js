7c0ddffe168af89d7f98154cd85aefd42588b96f
MaxTT-Billing <treadstonesolutions2023@gmail.com>
2025-09-10 14:27:45 +0530

    Create admin.latest.invoices.js

---
+++ routes/admin.latest.invoices.js
@@ -0,0 +1,8 @@
+[{ 
+  "id": 140,
+  "created_at": "...",
+  "franchisee_code": "TS-HR-GGM-001",
+  "printed_no": "TS-HR-GGM-001/0925/0019",
+  "norm_no": "TS-HR-GGM-001-0019",
+  "customer_code": "C000140"
+}]
